e1f32574f6b596c9f7d35d626a7c18e6a39468ba
Friedrich von Never <friedrich@fornever.me>
2024-02-01 23:21:59 +0100

    (#4) RTL_USER_PROCESS_PARAMETERS: update to use the InlineArray

--- ProcessDoctor.Backend.Windows/NT/RTL_USER_PROCESS_PARAMETERS.cs
+++ ProcessDoctor.Backend.Windows/NT/RTL_USER_PROCESS_PARAMETERS.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using PInvoke;
 
@@ -8,11 +9,17 @@ public unsafe struct RTL_USER_PROCESS_PARAMETERS
 {
     private fixed byte Reserved1[16];
 
-    private fixed long Reserved2[10];
+    private PVoidArray10 Reserved2;
 
     /// <summary>The path of the image file for the process.</summary>
     public NTDll.UNICODE_STRING ImagePathName;
 
     /// <summary>The command-line string passed to the process.</summary>
     public NTDll.UNICODE_STRING CommandLine;
+}
+
+[InlineArray(10)]
+public struct PVoidArray10
+{
+    private IntPtr _element0;
 }
