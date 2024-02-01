942ae71be1d86dbb7e4033b46dfb881fbe1d0ef8
Никита Великий <mtyoungstz@outlook.com>
2024-02-01 12:59:00 +0500

    Improve UNICODE_STRING handling performance

--- ProcessDoctor.Backend.Windows/NT/Extensions/UnicodeStringExtensions.cs
+++ ProcessDoctor.Backend.Windows/NT/Extensions/UnicodeStringExtensions.cs
@@ -1,21 +1,40 @@
-using System.Text;
 using PInvoke;
+using ProcessDoctor.Backend.Windows.Exceptions;
 
 namespace ProcessDoctor.Backend.Windows.NT.Extensions;
 
 internal static class UnicodeStringExtensions
 {
-    internal static string? ToManagedString(this NTDll.UNICODE_STRING unmanagedString, ProcessMemoryReader memoryReader)
+    internal static unsafe string? ToManagedString(this NTDll.UNICODE_STRING unmanagedString, Kernel32.SafeObjectHandle processHandle)
     {
         if (unmanagedString.Length == 0 || unmanagedString.MaximumLength == 0)
         {
             return null;
         }
 
-        var bytes = memoryReader.ReadBytes(
-            unmanagedString.Buffer_IntPtr,
-            unmanagedString.Length);
+        if (unmanagedString.Length % 2 != 0)
+        {
+            // malformed UTF-16 string
+            throw new CorruptedProcessHandleException();
+        }
 
-        return Encoding.Unicode.GetString(bytes);
+        var buffer = stackalloc char[unmanagedString.Length];
+        var isSuccess = Kernel32.ReadProcessMemory(
+            processHandle,
+            unmanagedString.Buffer_IntPtr,
+            (IntPtr)buffer,
+            unmanagedString.Length,
+            out var totalLength);
+
+        if (!isSuccess || unmanagedString.Length != totalLength)
+        {
+            throw new CorruptedProcessHandleException();
+        }
+
+        var span = new ReadOnlySpan<char>(
+            buffer,
+            unmanagedString.Length / 2);
+
+        return new string(span);
     }
 }
