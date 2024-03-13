b434d12fe62c336105cba7c44891f651d8b15ffd
Никита Великий <mtyoungstz@outlook.com>
2024-03-14 02:34:25 +0500

    Get rid of an unnecessary comment

--- ProcessDoctor.Backend.Windows/Imaging/Native/Shell32.cs
+++ ProcessDoctor.Backend.Windows/Imaging/Native/Shell32.cs
@@ -4,7 +4,6 @@ namespace ProcessDoctor.Backend.Windows.Imaging.Native;
 
 internal static class Shell32
 {
-    // For some reason this method is not supported by CsWin32: https://github.com/microsoft/CsWin32/issues/1159
     [DllImport("shell32.dll")]
     public static extern HRESULT SHGetStockIconInfo(IconType siid, IconFlags uFlags, ref SH_STOCK_ICON_INFO psii);
 }
