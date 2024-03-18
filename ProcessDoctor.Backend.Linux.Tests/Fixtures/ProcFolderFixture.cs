f7ae00015d67a028283246e99ca7de056f54be5a
Никита Великий <mtyoungstz@outlook.com>
2024-03-18 20:06:58 +0500

    (#26) Add tests for ExecutablePath property

--- ProcessDoctor.Backend.Linux.Tests/Fixtures/ProcFolderFixture.cs
+++ ProcessDoctor.Backend.Linux.Tests/Fixtures/ProcFolderFixture.cs
@@ -6,8 +6,6 @@ namespace ProcessDoctor.Backend.Linux.Tests.Fixtures;
 [UsedImplicitly]
 public sealed class ProcFolderFixture
 {
-    public MockFileSystem FileSystem { get; } = new();
-
     public ProcessFixture CreateProcess(uint id)
-        => new(FileSystem, id);
+        => new(new MockFileSystem(), id);
 }
